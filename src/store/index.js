ded4fb6495e5d1edc3dc17335eae5a4d18e7dbde
gonska93 <natalia.goska@gmail.com>
2023-01-02 15:22:07 +0100

    change to hook

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import TasksStore from 'store/TasksStore';
 import CardsStore from 'store/CardsStore';
-import { createContext, useContext } from 'react';
 
 const stores = {
     tasksStore: new TasksStore(),
     cardsStore: new CardsStore()
 };
 
-const storeContext = createContext(stores);
-export const useStores = () => useContext(storeContext);
+export const useStores = () => stores;
