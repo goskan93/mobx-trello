307561d81b3cb567e2a1ab8ab69ed296f84ff572
gonska93 <natalia.goska@gmail.com>
2023-06-15 12:57:00 +0200

    index

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,6 +5,18 @@ const Layout = ({ children }) => {
     return (
         <Provider theme={defaultTheme}>
             <main>{children}</main>
+            <footer>
+                <p style={{ fontSize: '0.5rem' }}>
+                    If you have any suggestions, please add an issue{' '}
+                    <a
+                        href='https://github.com/goskan93/mobx-trello/issues'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        here.
+                    </a>{' '}
+                </p>
+            </footer>
         </Provider>
     );
 };
